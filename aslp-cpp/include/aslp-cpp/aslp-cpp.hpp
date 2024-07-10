29d563028968c34c171202bf876818258f4b8e4b
rina <k@rina.fyi>
2024-07-10 15:26:36 +1000

    aslp-cpp: support extra query params

--- aslp-cpp/include/aslp-cpp/aslp-cpp.hpp
+++ aslp-cpp/include/aslp-cpp/aslp-cpp.hpp
@@ -1,35 +1,41 @@
 #pragma once
 
-#include <string>
+#include <map>
 #include <memory>
+#include <string>
+#include <vector>
 
-namespace httplib {
+namespace httplib
+{
 class Client;
-} // namespace httplib;
+}  // namespace httplib
 
 // tuple of encoding and semantics
 using aslp_opcode_result_t = std::tuple<std::string, std::string>;
 
 class aslp_connection
 {
+  using params_t = std::multimap<std::string, std::string>;
+
   std::unique_ptr<httplib::Client> client {nullptr};
+  const params_t& extra_params;
 
 public:
-  aslp_connection(const std::string& server_addr, int server_port);
+  aslp_connection(const std::string& server_addr, int server_port, const params_t& extra_params = {});
   aslp_connection(aslp_connection&&) noexcept;
   auto get_opcode(uint32_t opcode) -> aslp_opcode_result_t;
   void wait_active();
   ~aslp_connection();
 };
 
-
-
-class aslp_client {
+class aslp_client
+{
 private:
   const std::string server_addr;
   pid_t server_pid;
   int server_port;
   void shutdown();
+
 public:
   aslp_client(const aslp_client&) = delete;
   aslp_client(aslp_client&&) = delete;
@@ -43,26 +49,26 @@ public:
       : server_pid(pid)
       , server_port(port)
       , server_addr(std::move(addr))
-  { }
+  {
+  }
 
   /**
    * Creates a new aslp_client with a managed server on
    * the default address, localhost:8000.
    */
-  std::unique_ptr<aslp_client> static start() {
+  std::unique_ptr<aslp_client> static start()
+  {
     return start("127.0.0.1", 8000);
   }
 
   /** Creates a new managed aslp_client with the given address and port. */
-  auto static start(const std::string& addr, int server_port) -> std::unique_ptr<aslp_client>;
+  auto static start(const std::string& addr,
+                    int server_port) -> std::unique_ptr<aslp_client>;
 
-  /** Returns the semantics for the given opcode, as a newline-separated string. */
+  /** Returns the semantics for the given opcode, as a newline-separated string.
+   */
   auto get_opcode(uint32_t opcode) -> aslp_opcode_result_t;
 
   /** Destroys the aslp_client and terminates the managed server as well. */
-  virtual ~aslp_client() {
-    shutdown();
-  };
-
+  virtual ~aslp_client() { shutdown(); };
 };
-
